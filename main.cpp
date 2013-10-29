4a06013fe71ff5dad80659d6718da4579fa072ef
Fredrik Teschke <f@ftes.de>
2013-10-29 11:12:53 +0100

    created netbeans project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fredrik
+ *
+ * Created on October 29, 2013, 11:05 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+

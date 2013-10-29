09ab40e83d53fd388109260a680697765d7cb610
Fredrik Teschke <f@ftes.de>
2013-10-29 11:18:41 +0100

    added pthreads library and thread example

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
-/* 
- * File:   main.cpp
- * Author: fredrik
- *
- * Created on October 29, 2013, 11:05 AM
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-#include <cstdlib>
+void *print_message_function( void *ptr );
 
-using namespace std;
+main()
+{
+     pthread_t thread1, thread2;
+     const char *message1 = "Thread 1";
+     const char *message2 = "Thread 2";
+     int  iret1, iret2;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+    /* Create independent threads each of which will execute function */
 
-    return 0;
+     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+     iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+
+     /* Wait till threads are complete before main continues. Unless we  */
+     /* wait we run the risk of executing an exit which will terminate   */
+     /* the process and all threads before the threads have completed.   */
+
+     pthread_join( thread1, NULL);
+     pthread_join( thread2, NULL); 
+
+     printf("Thread 1 returns: %d\n",iret1);
+     printf("Thread 2 returns: %d\n",iret2);
+     exit(0);
 }
 
+void *print_message_function( void *ptr )
+{
+     char *message;
+     message = (char *) ptr;
+     printf("%s \n", message);
+}

f4587333e571ebeb9cf0a30db9d415fb9da0e4b6
Fredrik Teschke <f@ftes.de>
2013-10-31 01:02:44 +0100

    edited archive script

--- create-archive.sh
+++ create-archive.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -f parsum.tar
-git archive --format=tar HEAD > parsum.tar
+
+res=$(git status | grep "nothing to commit, working directory clean" -c)
+
+if [[ $res == "0" ]]; then
+	echo "ERROR: unsynched changes with git repo"
+else
+	git archive --format=tar HEAD > parsum.tar
+	echo "Done"
+fi

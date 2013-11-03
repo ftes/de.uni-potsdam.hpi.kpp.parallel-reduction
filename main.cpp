47c3927dd18f82e1f67bfa2a28d24e492f41cdf7
Fredrik Teschke <f@ftes.de>
2013-11-03 19:46:26 +0100

    changed to simple output of input parameters

--- main.cpp
+++ main.cpp
@@ -1,42 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <iostream>
 
+using namespace std;
 
 void *print_message_function( void *ptr );
 
-main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-    unsigned int noThreads = atoi(argv[1]);
-    unsigned long long startIndex = strtoull(argv[2], 0, 10);
-    unsigned long long endIndex = strtoull(argv[3], 0, 10);
+    const unsigned int noThreads = atoi(argv[1]);
+    const unsigned long long startIndex = strtoull(argv[2], 0, 10);
+    const unsigned long long endIndex = strtoull(argv[3], 0, 10);
     
-     printf("Number of threads: %d, Start: %llu, End: %llu\n", noThreads, startIndex, endIndex);
-     pthread_t thread1, thread2;
-     const char *message1 = "Thread 1";
-     const char *message2 = "Thread 2";
-     int  iret1, iret2;
+    printf("Threads: %d, Start: %llu, End %llu\n", noThreads, startIndex, endIndex);
+    
+    cout << "test\n";
 
-    /* Create independent threads each of which will execute function */
-
-     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
-     iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
-
-     /* Wait till threads are complete before main continues. Unless we  */
-     /* wait we run the risk of executing an exit which will terminate   */
-     /* the process and all threads before the threads have completed.   */
-
-     pthread_join( thread1, NULL);
-     pthread_join( thread2, NULL); 
-
-     printf("Thread 1 returns: %d\n",iret1);
-     printf("Thread 2 returns: %d\n",iret2);
-     exit(0);
-}
-
-void *print_message_function( void *ptr )
-{
-     char *message;
-     message = (char *) ptr;
-     printf("%s \n", message);
+    exit(0);
 }
